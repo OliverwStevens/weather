32e28c7a3b786ce08ad5199eca242c9ebce8b14a
OliverwStevens <oliverwstevens@gmail.com>
2025-01-09 11:38:36 -0500

    4th Commit, handled API Error

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -24,6 +24,37 @@ class LocationsController < ApplicationController
     if @lat.present? && @lng.present?
       @highs, @lows = forecast
       puts "Highs: #{@highs} Lows: #{@lows}"
+      highs_data = @highs.join(",")
+      lows_data = @lows.join(",")
+
+
+      
+      #@chart_url = "https://image-charts.com/chart?cht=bvg&chbr=10&chs=700x100&chd=t:#{highs_data}|#{lows_data}&chco=E5446D,60B2E5"
+      #@chart_url = "https://image-charts.com/chart?chbh=20&chbr=10&chco=E5446D%2C60B2E5&chd=t:#{highs_data}|#{lows_data}&chdl=Highs%7CLows&chdlp=r&chg=5&chs=700x300&cht=bvg&chtt=Temperature&chxl=0%3A%7CJan%7CFev%7CMar%7CAvr%7CMay&chxt=x%2Cy"
+
+
+      require 'date'
+
+      # Get today's date
+      today = Date.today
+
+      # Array to hold the day names
+      day_names = []
+
+      # Loop to get today and the next 6 days
+      (0..6).each do |i|
+        day_names << (today + i).strftime("%A")
+      end
+
+      # Join the day names with | for the URL
+      day_names_str = day_names.join('%7C')
+
+      # Update the @chart_url
+      @chart_url = "https://image-charts.com/chart?chbh=20&chbr=10&chco=E5446D%2C60B2E5&chd=t:#{highs_data}|#{lows_data}&chdl=Highs%7CLows&chdlp=r&chg=5&chs=700x300&cht=bvg&chtt=Temperature&chxl=0%3A%7C#{day_names_str}&chxt=x%2Cy"
+
+
+
+
     end
   end
 
@@ -108,10 +139,22 @@ class LocationsController < ApplicationController
     end
 
     def forecast
-      url = "https://api.open-meteo.com/v1/forecast?latitude=#{@lat}&longitude=#{@lng}&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch"
-      uri = URI(url)
-      response = Net::HTTP.get(uri)
-      report = JSON.parse(response)
-      [report["daily"]["temperature_2m_max"], report["daily"]["temperature_2m_min"]]
+      
+      if @lat.present? && @lng.present? && valid_lat_lng?(@lat, @lng)
+        url = "https://api.open-meteo.com/v1/forecast?latitude=#{@lat}&longitude=#{@lng}&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch"
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        report = JSON.parse(response)
+        [report["daily"]["temperature_2m_max"], report["daily"]["temperature_2m_min"]]
+      else
+        # Handle invalid or missing latitude and longitude
+        Rails.logger.warn("Invalid or missing latitude/longitude: lat=#{@lat}, lng=#{@lng}")
+      end
+      
     end
+
+    def valid_lat_lng?(lat, lng)
+      lat.to_f.between?(-90, 90) && lng.to_f.between?(-180, 180)
+    end
+    
 end
