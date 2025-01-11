ee4e2053c9dcbf548afcd2141c8b16a0a3060ca8
OliverwStevens <oliverwstevens@gmail.com>
2025-01-11 14:07:04 -0500

    Added support for IPv6

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ApplicationRecord
   validates :zipcode, format: { with: /\A\d{5}\z/, message: "Must be a valid US ZIP code." }, allow_blank: true
-  validates :ip_address, format: { with: Resolv::IPv4::Regex, message: "Must be a valid IPv4 address" }, allow_blank: true
+  validates :ip_address, format: { with: /\A(?:#{Resolv::IPv4::Regex}|#{Resolv::IPv6::Regex})\z/, message: "Must be a valid IPv4 or IPv6 address" }, allow_blank: true
   validate :zipcode_or_ip_address_present
 
   private
